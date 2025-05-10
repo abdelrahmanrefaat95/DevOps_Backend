17a6c780b78adfcc4e1f6281814f5e4c53f0d8ac
ShoroukNabil <38180573+ShoroukNabil@users.noreply.github.com>
2025-05-10 16:04:39 +0300

    Update WebConfig.java

--- src/main/java/com/devops/devops/WebConfig.java
+++ src/main/java/com/devops/devops/WebConfig.java
@@ -10,10 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // Allow all paths
-                .allowedOrigins(
-                        "http://localhost:4200",       // Angular dev server
-                        "http://127.0.0.1:4200"     // Fallback for some environments
-                )
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true);
     }
